010eacdc1d0bd8a2e21680c8b4dc7cb50ab22505
Amerlan <Amerlan-t@bk.ru>
2020-10-01 20:36:06 +0600

    Working sign, reject and return processes

--- resources/views/ongoing.blade.php
+++ resources/views/ongoing.blade.php
@@ -8,15 +8,18 @@
 @foreach($documents as $document)
     <h3>doc id: {{$document->document_id}}</h3>
     <h2>doc type: {{$document->document_type}}</h2>
-    <h4>executor: {{$document->executor_id}}</h4>
-    <h4>Initiator: {{$document->created_by}}</h4>
+    <h4>executor: {{$document->ename}} {{$document->executor_id}}</h4>
+    <h4>Initiator: {{$document->cname}} {{$document->created_by}}</h4>
     <h4>Current stage: {{$document->current_stage}}</h4>
     <h4 style="@if($document->is_rejected)background-color:green; @else background-color:red; @endif">Is rejected: {{$document->is_rejected}}</h4>
     <h5>Created date: {{$document->created_date}}</h5>
     <h5>Signed date: {{$document->signed_date}}</h5>
     <h5>Last change date: {{$document->last_change_date}}</h5>
     <h4 style="@if($document->is_closed)background-color:green; @else background-color:red; @endif">Is closed: {{$document->is_closed}}</h4>
+    <a href="{{route('sign', ['doc_id'=>$document->document_id])}}"><h2>Sign</h2></a>
+    <a href="{{route('reject', ['doc_id'=>$document->document_id])}}"><h2>Reject</h2></a>
+    <a href="{{route('return', ['doc_id'=>$document->document_id])}}"><h2>Return</h2></a>
     <hr>
-@endforeach
 </body>
+@endforeach
 </html>
