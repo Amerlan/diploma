d167c2ed122c3162028f3ee3e504acf4c58861ae
madiyar2403 <madiyar2403@mail.ru>
2020-10-06 19:28:13 +0300

    Logout button bug fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -268,10 +268,9 @@
                            onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                             <i class="nav-icon far fa-circle text-danger"></i>
-                            {{ __('Logout') }}
+                            <p>{{ __('Logout') }}</p>
                         </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
                         </form>
                     </li>
                 </ul>
