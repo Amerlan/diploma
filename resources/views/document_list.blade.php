6dc9a8c906d393938434565d5cfaecde54c33734
Amir <amirzhan.tolebay@gmail.com>
2020-10-06 21:12:56 +0600

    Tuned Documents list

--- resources/views/document_list.blade.php
+++ resources/views/document_list.blade.php
@@ -1,18 +1,6 @@
 @extends('layouts.admin')
+
 @section('content')
-       <!-- @foreach($documents as $document)
-            <h3>doc id: {{$document->document_id}}</h3>
-            <h2>doc type: {{$document->document_type}}</h2>
-            <h4>executor: {{$document->ename}} {{$document->executor_id}}</h4>
-            <h4>Initiator: {{$document->cname}} {{$document->created_by}}</h4>
-            <h4>Current stage: {{$document->current_stage}}</h4>
-            <h4 style="@if($document->is_rejected)background-color:green; @else background-color:red; @endif">Is rejected: {{$document->is_rejected}}</h4>
-            <h5>Created date: {{$document->created_date}}</h5>
-            <h5>Signed date: {{$document->signed_date}}</h5>
-            <h5>Last change date: {{$document->last_change_date}}</h5>
-            <h4 style="@if($document->is_closed)background-color:green; @else background-color:red; @endif">Is closed: {{$document->is_closed}}</h4>
-            <hr>
-        @endforeach-->
     <div class="card-body">
         <table id="example1" class="table table-bordered table-striped">
             <thead>
@@ -37,11 +25,11 @@
                     <td>{{$document->ename}} {{$document->executor_id}}</td>
                     <td>{{$document->cname}} {{$document->created_by}}</td>
                     <td>{{$document->current_stage}}</td>
-                    <td>{{$document->is_rejected}}</td>
+                    @if($document->is_rejected == 1) <td>Yes <i class="fas fa-circle" style="color:firebrick"></i> </td>  @else  <td>No <i class="fas fa-circle" style="color:green"></i></td> @endif
                     <td>{{$document->created_date}}</td>
                     <td>{{$document->signed_date}}</td>
                     <td>{{$document->last_change_date}}</td>
-                    <td>{{$document->is_closed}}</td>
+                    @if($document->is_closed == 1) <td>Yes <i class="fas fa-circle" style="color:firebrick"></i> </td>  @else  <td>No <i class="fas fa-circle" style="color:green"></i></td> @endif
                 </tr>
             @endforeach
             </tbody>
