1759a88ba5352434226aff52b23a726b5b4eacea
madiyar2403 <madiyar2403@mail.ru>
2020-10-30 23:57:48 +0300

    New template added to main page, login, logout works. Added new files for design(css, vendor,img, etc.).

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -163,7 +163,7 @@
                     <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">{{ Auth::user()->email }}</a>
+                        <a href="#" class="d-block">{{ Auth::user()->email }}</a>
                 </div>
             </div>
             <!-- Sidebar Menu -->
