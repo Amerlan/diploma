ac5a90b6835cfafc8c03fad44bf4c5d76bc0a5ac
madiyar2403 <madiyar2403@mail.ru>
2020-11-04 23:34:30 +0300

    Добавил вкладку всех статусов документов.

--- resources/views/ongoing.blade.php
+++ resources/views/ongoing.blade.php
@@ -7,13 +7,13 @@
             <tr>
                 <th>ID</th>
                 <th>Document type</th>
-                <th>Executor id</th>
+                <th>Executor Name</th>
                 <th>Created By</th>
                 <th>Current Stage</th>
-                <th>Is rejected</th>
                 <th>Created date</th>
                 <th>Signed date</th>
                 <th>Last change date</th>
+                <th>Is rejected</th>
                 <th>Is closed</th>
             </tr>
             </thead>
@@ -25,10 +25,10 @@
                     <td>{{$document->executor_role}} {{$document->executor_role_id}}</td>
                     <td>{{$document->name}} {{$document->created_by}}</td>
                     <td>{{$document->current_stage}}</td>
-                    @if($document->is_rejected == 1) <td>Yes <i class="fas fa-circle" style="color:firebrick"></i> </td>  @else  <td>No <i class="fas fa-circle" style="color:green"></i></td> @endif
                     <td>{{$document->created_date}}</td>
                     <td>{{$document->signed_date}}</td>
                     <td>{{$document->last_change_date}}</td>
+                    @if($document->is_rejected == 1) <td>Yes <i class="fas fa-circle" style="color:firebrick"></i> </td>  @else  <td>No <i class="fas fa-circle" style="color:green"></i></td> @endif
                     @if($document->is_closed == 1) <td>Yes <i class="fas fa-circle" style="color:firebrick"></i> </td>  @else  <td>No <i class="fas fa-circle" style="color:green"></i></td> @endif
                 </tr>
             @endforeach
