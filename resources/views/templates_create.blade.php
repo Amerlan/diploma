4de57f3f547d41e9c8dd54922adfec5a957b569f
madiyar2403 <madiyar2403@mail.ru>
2020-12-21 18:27:17 +0300

    Multilanguage added to templates_create.blade.php.

--- resources/views/templates_create.blade.php
+++ resources/views/templates_create.blade.php
@@ -16,92 +16,89 @@
         @csrf
         <div class="col-sm-12" style="float:none;margin:auto;padding-top: 1%">
             <div class="card card-primary">
-                <div class="card-header">
-                    <h3 class="card-title">Create document template</h3>
-                </div>
                     <div class="card-body">
                         <div class="form-group">
-                            <label>Document type</label>
-                            <input type="text" class="form-control" name="document_name" placeholder="Enter document name">
+                            <label>@lang('messages.document_name')</label>
+                            <input type="text" class="form-control" name="document_name" placeholder="@lang('messages.enter_document_name')">
                         </div>
                         <div class="form-group">
-                            <label>Stage count</label>
-                            <input type="text" class="form-control" name="stageCount" placeholder="Enter stage count">
+                            <label>@lang('messages.stage_count')</label>
+                            <input type="text" class="form-control" name="stageCount" placeholder="@lang('messages.enter_stage_count')">
                         </div>
                         <div class="form-group">
-                            <label>Enter document header</label>
-                            <input type="text" class="form-control" name="header" placeholder="Enter header that will be displayed">
+                            <label>@lang('messages.enter_doc_header')</label>
+                            <input type="text" class="form-control" name="header" placeholder="@lang('messages.enter_doc_header_displayed')">
                         </div>
                         <div class="form-group">
-                            <label>Enter document body</label>
-                            <input type="text" class="form-control" name="body" placeholder="Enter body that will be displayed">
+                            <label>@lang('messages.enter_doc_body')</label>
+                            <input type="text" class="form-control" name="body" placeholder="@lang('messages.enter_doc_body_displayed')">
                         </div>
                         <!-- Checkboxes menu -->
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="reason" id="reason">
-                            <label class="form-check-label" for="reason">Reason</label>
+                            <label class="form-check-label" for="reason">@lang('messages.reason')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="new_fio" id="new_fio">
-                            <label class="form-check-label" for="new_fio">New FIO</label>
+                            <label class="form-check-label" for="new_fio">@lang('messages.new_full_name')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="new_speciality" id="new_speciality">
-                            <label class="form-check-label" for="new_speciality">New speciality</label>
+                            <label class="form-check-label" for="new_speciality">@lang('messages.new_speciality')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="new_speciality_code" id="new_speciality_code">
-                            <label class="form-check-label" for="new_speciality_code">New speciality code</label>
+                            <label class="form-check-label" for="new_speciality_code">@lang('messages.new_speciality_code')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="sum_of_return" id="sum_of_return">
-                            <label class="form-check-label" for="sum_of_return">Sum of return</label>
+                            <label class="form-check-label" for="sum_of_return">@lang('messages.sum_of_return')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="new_university" id="new_university">
-                            <label class="form-check-label" for="new_university">New university</label>
+                            <label class="form-check-label" for="new_university">@lang('messages.new_university')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="academic_year" id="academic_year">
-                            <label class="form-check-label" for="academic_year">Academic year</label>
+                            <label class="form-check-label" for="academic_year">@lang('messages.academic_year')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="subject" id="subject">
-                            <label class="form-check-label" for="subject">Subject</label>
+                            <label class="form-check-label" for="subject">@lang('messages.subject')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="midterm_grade" id="midterm_grade">
-                            <label class="form-check-label" for="midterm_grade">Midterm grade</label>
+                            <label class="form-check-label" for="midterm_grade">@lang('messages.midterm_grade')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="endterm_grade" id="endterm_grade">
-                            <label class="form-check-label" for="endterm_grade">Endterm grade</label>
+                            <label class="form-check-label" for="endterm_grade">@lang('messages.endterm_grade')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="exam_grade" id="exam_grade">
-                            <label class="form-check-label" for="exam_grade">Exam grade</label>
+                            <label class="form-check-label" for="exam_grade">@lang('messages.exam_grade')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="teacher" id="teacher">
-                            <label class="form-check-label" for="teacher">Teacher</label>
+                            <label class="form-check-label" for="teacher">@lang('messages.teacher')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="semester" id="semester">
-                            <label class="form-check-label" for="semester">Semester</label>
+                            <label class="form-check-label" for="semester">@lang('messages.semester')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="phone_number" id="phone_number">
-                            <label class="form-check-label" for="phone_number">Phone number</label>
+                            <label class="form-check-label" for="phone_number">@lang('messages.phone_number')</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="attachments" id="attachments">
-                            <label class="form-check-label" for="attachments">Attachments</label>
+                            <label class="form-check-label" for="attachments">@lang('messages.attachments')</label>
                         </div>
                         <!-- Checkboxes menu end -->
                     </div>
                     <div class="card-footer">
-                        <input type="submit" value="Submit" class="btn btn-primary">
-                        <a class="btn btn-default float-right" href="{{URL::to('/')}}">Cancel</a>
+                        <input type="submit" value="@lang('messages.submit')" class="btn btn-primary">
+                        <a class="btn btn-default float-right" href="{{URL::to('/')}}">@lang('messages.cancel')</a>
                     </div>
             </div>
         </div>
